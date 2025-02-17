91c7ef930b8c9c5e6cba9149622424204c03d78b
KIM <wltn9169@naver.com>
2025-02-17 18:29:29 +0900

    "Initial commit"

---
+++ src/test/java/com/jisu9169/boardproject/boardproject/BoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jisu9169.boardproject.boardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
